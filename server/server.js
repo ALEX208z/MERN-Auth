19662cbdc784249188b2566d44e74ea3aeb8e86f
Ayush <anshumanayush208@gmail.com>
2026-03-02 00:17:21 +0530

    Add authentication routes for user registration, login, and logout

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
+import authRouter from './routes/authRoutes.js'
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -13,9 +14,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}))
 
+
+// API endpoints
 app.get('/', (req, res) => {
   res.send("API working good")
 })
+app.use('/api/auth', authRouter)
 
 app.listen(port,() => {
   console.log(`Server started on PORT:${port}`);
