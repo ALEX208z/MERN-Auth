588d2fc135c2bef8c4a847a613cfa470fcb997de
Ayush <anshumanayush208@gmail.com>
2026-03-06 00:38:10 +0530

    Add user routes and controller for fetching user data

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./config/mongodb.js";
 import authRouter from './routes/authRoutes.js'
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
   res.send("API working good")
 })
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.listen(port,() => {
   console.log(`Server started on PORT:${port}`);
