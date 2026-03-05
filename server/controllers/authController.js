0904898b80b51f2b587398b9e2a6a071dd540b41
Ayush <anshumanayush208@gmail.com>
2026-03-05 17:42:31 +0530

    Add nodemailer configuration and send welcome email on user registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import transporter from "../config/nodemailer.js";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -32,8 +33,17 @@ export const register = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    return res.json({success: true});
+    // Sending welcome email
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome",
+      text: `Welcome to the website. Your account has been craeted with email id: ${email}`,
+    };
 
+    await transporter.sendMail(mailOptions);
+
+    return res.json({ success: true });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
@@ -73,27 +83,22 @@ export const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    return res.json({success: true});
-
-
+    return res.json({ success: true });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
 };
 
-
-
-export const logout = async(req, res) => {
+export const logout = async (req, res) => {
   try {
-    res.clearCookie('token', {
+    res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    })
+    });
 
-    return res.json({success: true, message: "Logged out"});
-    
+    return res.json({ success: true, message: "Logged out" });
   } catch (error) {
-    return res.json({success: false, message: error.message});
+    return res.json({ success: false, message: error.message });
   }
-}
+};
