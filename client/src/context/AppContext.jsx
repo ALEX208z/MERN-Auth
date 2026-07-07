eed410bb4a9e4beacd5e8d68593f84f9083bb89a
Ayush <anshumanayush208@gmail.com>
2026-07-07 13:13:49 +0530

    feat: enhance Navbar component to display user initials and add user actions based on authentication state

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,35 +1,56 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
-import axios from 'axios';
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userData, setUserData] = useState(false);
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const [userData, setUserData] = useState(false)
+  const getUserData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/data");
+      data.success ? setUserData(data.userData) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-    const getUserData = async() => {
-        try {
-            const {data} = await axios.get(backendUrl + '/api/user/data')
-            data.success ? setUserData(data.userData) : toast.error(data.message)
-        } catch (error) {
-            toast.error(error.message)
+  const getAuthState = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth");
+      if (data.success) {
+        setIsLoggedIn(true);
+        const { data: userInfo } = await axios.get(backendUrl + "/api/user/data");
+        if (userInfo.success) {
+          setUserData(userInfo.userData);
         }
+      }
+    } catch (error) {
+      console.log(error.message);
     }
+  };
 
-    const value = {
-        backendUrl,
-        isLoggedIn, setIsLoggedIn,
-        userData, setUserData,
-        getUserData
-    }
+ // eslint-disable-next-line react-hooks/exhaustive-deps
+useEffect(() => {
+  getAuthState();
+}, []);
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const value = {
+    backendUrl,
+    isLoggedIn,
+    setIsLoggedIn,
+    userData,
+    setUserData,
+    getUserData,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
