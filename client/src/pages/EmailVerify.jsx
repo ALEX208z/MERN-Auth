6aa3513aeac68f337ec1064d40d2832a54b4a14a
Ayush <anshumanayush208@gmail.com>
2026-07-30 23:42:34 +0530

    feat: enhance email verification component with input handling and paste functionality

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -3,11 +3,39 @@ import { assets } from "../assets/assets";
 import { useNavigate  } from "react-router-dom";
 
 const EmailVerify = () => {
+
+  const navigate = useNavigate();
+
+  const inputRefs = React.useRef([])
+
+  const handleInput = (e, index) => {
+    if(e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  }
+
+  const handleKeyDown = (e, index) => {
+    if(e.key === 'Backspace' && e.target.value === '' && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  }
+
+  const handlePaste = (e) => {
+    e.preventDefault() // ✅ stops browser from pasting into just one input
+    const paste = e.clipboardData.getData('text')
+    const pasteArray = paste.split('');
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    })
+  } 
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-blue-200 to-purple-400">
       {/* <h1>Email Verify</h1> */}
       <img
-        onClick={() => useNavigate ("/")}
+        onClick={() => navigate ("/")}
         className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"
         src={assets.logo}
         alt=""
@@ -19,6 +47,15 @@ const EmailVerify = () => {
         <p className="text-center mb-6 text-indigo-300">
           Enter the 6-digit code sent to your email id
         </p>
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+            {Array(6).fill(0).map((_, index) => (
+              <input type="text" maxLength='1' key={index} required className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+              ref={e => inputRefs.current[index] = e}
+              onInput={(e) => handleInput(e, index)}
+              onKeyDown={(e) => handleKeyDown(e, index)}/>
+            ))}
+        </div>
+        <button className="w-full py-3 bg-linear-to-r from-indigo-500 to-indigo-900 text-white rounded-full">Verify email</button>
       </form>
     </div>
   );
